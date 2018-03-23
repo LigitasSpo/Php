6e434ee3b06468fa81b083c69b2ce2e2adbf5cfd
CodeAcademy <ligitas.spo@gmail.com>
2018-03-23 09:22:11 +0200

    test

---
+++ PHP/10.6.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: CodeAcademy
+ * Date: 3/23/2018
+ * Time: 8:22 AM
+ */
+/*
+Sukurkite PHP skriptą, kuriame aprašykite klasę loto,
+kurioje būtų viena savybė ‐ $table, kuri bus dvimatis masyvas,
+ taip pat būtų metodas generate(), kuris sugeneruoja 5 x 5
+ atsitiktinių skaičių dvimatį masyvą (min skaičius = 1, max skaičius = 75).
+ Sukurkite metodą get(),kuris išveda sugeneruotą masyvą 5 x 5 HTML lentele. Pademonstruokite veikimą.
+*/
+
+
+class table {
+   public $m = [];
+
+   function  generate(){
+       for ($x = 0; $x < 5; $x++){
+
+           $z =[];
+
+           for ($c = 0; $c <5; $c++){
+               $z[] = rand(1, 75);
+           }
+
+           $this->m[] = $z;
+       }
+
+
+   }
+}
+
+
+
+$p = new table();
+$p->generate();
+
